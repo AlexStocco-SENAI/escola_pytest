25b09f55c5a34a38b007e0a120dc9e9136be49b2
Ivo <ivo.neto603@gmail.com>
2025-08-12 11:22:41 -0300

    .

--- escola/aluno.py
+++ escola/aluno.py
@@ -1,8 +1,32 @@
 def calcular_media(notas:list[float]) -> float:
+    """" 
+    Calcula a média de uma lista de notas.
+
+    Parâmetros:
+    notas (list[float]): Lista de notas a serem calculadas.
+    
+    Retorna:
+    float: A média das notas, arredondada para uma casa decimal.
+    """
+
+    #validando se a entrada é uma lista
+    if not isinstance(notas, list):
+        raise TypeError("nota invalida")
+
+    #validando se a entrada é uma lista 2.0
+    if type(notas) != list:
+        raise TypeError("nota invalida")
 
     #validando se a lista é vazia
-    if len(notas) == 0:
+    if len(notas) == 10:
         raise ValueError("não é permitido uma lista vazia")
+    
+    #validando se todos os elementos da lista são números (int ou float)
+    for nota in notas:
+        if not type(nota) == int and not type(nota) == float:
+            raise TypeError("nota invalida")
+        if nota < 0 or nota > 10:
+            raise ValueError("limite da nota [0, 10]")
     
     media = sum(notas)/len(notas)
     return round(media,1)
